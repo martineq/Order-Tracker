94713cccc88d2fa907fc62b36c0b67947b69e9ae
mhgomez <martinhgomez@yahoo.com.ar>
2016-04-09 13:32:52 -0300

    ValidationService

---
+++ ordertracker/grails-app/services/ordertracker/ValidationService.groovy
@@ -0,0 +1,42 @@
+package ordertracker
+
+import ordertracker.protocol.ProtocolJsonBuilder
+import ordertracker.protocol.Result
+import ordertracker.protocol.Status
+import ordertracker.queries.Keywords
+import ordertracker.queries.QueryException
+import ordertracker.queries.Queryingly
+import ordertracker.queries.Requester
+import ordertracker.tranmission.TransmissionMedium
+
+class ValidationService implements Queryingly{
+
+    private def user
+    private boolean validationResult
+
+    ValidationService() {
+        this.user = new User(username: '', password: '', token: '')
+        this.validationResult = false
+    }
+
+    @Override
+    def validate(Requester requester) {
+        if ( requester.validateRequest(Arrays.asList(Keywords.USERNAME, Keywords.TOKEN)) == false )
+            throw new QueryException("Invalid request - user rejected")
+
+        this.user.username = requester.getProperty(Keywords.USERNAME)
+        this.user.token = requester.getProperty(Keywords.TOKEN)
+        return true
+    }
+
+    @Override
+    def generateQuery() {
+        this.validationResult = this.user.token.compareTo( User.findByUsername(this.user.username.toString()).token ) == 0 ? true : false
+        return this.validationResult
+    }
+
+    @Override
+    def obtainResponse(TransmissionMedium transmissionMedium) {
+        return new ProtocolJsonBuilder().addStatus(new Status(Result.defineResult(this.validationResult),"Permitted access"))
+    }
+}
---
+++ ordertracker/src/test/groovy/ordertracker/services/ValidationServiceSpec.groovy
@@ -0,0 +1,114 @@
+package ordertracker.services
+
+import grails.test.mixin.Mock
+import grails.test.mixin.TestFor
+import ordertracker.User
+import ordertracker.UserLoader
+import ordertracker.ValidationService
+import ordertracker.protocol.ProtocolJsonBuilder
+import ordertracker.queries.Keywords
+import ordertracker.queries.QueryException
+import ordertracker.queries.Requester
+import ordertracker.tranmission.DefaultTransmission
+import spock.lang.Specification
+
+@TestFor(ValidationService)
+@Mock(User)
+class ValidationServiceSpec extends Specification {
+
+    def setup() {
+        new UserLoader().load()
+    }
+
+    private Requester generateMartinRequester() {
+        Requester requester = new Requester()
+        requester.addProperty(Keywords.METHOD, Keywords.GET)
+        requester.addProperty(Keywords.USERNAME, 'martin')
+        requester.addProperty(Keywords.TOKEN, 'token1')
+        return requester
+    }
+
+    void "test validRequest"() {
+        given:
+            def requester = this.generateMartinRequester()
+
+        and:
+            def validationService = new ValidationService()
+
+        when:
+            def validationResult = validationService.validate(requester)
+
+        then:
+            validationResult == true
+    }
+
+    void "test invalidRequest"() {
+        given:
+        Requester requester = new Requester()
+        requester.addProperty(Keywords.METHOD, Keywords.GET)
+        requester.addProperty(Keywords.USERNAME, 'martin')
+
+        and:
+        def validationService = new ValidationService();
+
+        try {
+            when:
+                validationService.validate(requester)
+        }
+
+        catch (QueryException qe ) {
+            then:
+                qe.getMessage() == 'Invalid request - user rejected'
+        }
+    }
+
+    void "test invalidUsernameToken"() {
+        given:
+            Requester requester = new Requester()
+            requester.addProperty(Keywords.METHOD, Keywords.GET)
+            requester.addProperty(Keywords.USERNAME, 'martin')
+            requester.addProperty(Keywords.TOKEN, 'tokenM')
+
+        and:
+            def validationService = new ValidationService();
+
+        when:
+            validationService.validate(requester)
+            def validationResult = validationService.generateQuery()
+
+        then:
+            validationResult == false
+    }
+
+    void "test validUsernameToken"() {
+        given:
+            def requester = this.generateMartinRequester()
+
+        and:
+            def validationService = new ValidationService();
+
+        when:
+            validationService.validate(requester)
+            def validationResult = validationService.generateQuery()
+
+        then:
+            validationResult == true
+    }
+
+    void "test obtainValidJson"() {
+        given:
+            def requester = this.generateMartinRequester()
+
+        and:
+            def validationService = new ValidationService();
+
+        when:
+            validationService.validate(requester)
+            validationService.generateQuery()
+            ProtocolJsonBuilder Builder = validationService.obtainResponse(DefaultTransmission.obtainDefaultTransmission())
+
+        then:
+            Builder.build() == '{"status":{"result":"ok","description":"Permitted access"}}'
+    }
+
+}
