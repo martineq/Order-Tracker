cc92c260addbe54f53acd92f7c99608f925f3e78
Mart <martineq@gmail.com>
2016-04-09 21:05:16 -0300

    Agrego tabla Product

---
+++ grails/grails-app/domain/ordertracker/Product.groovy
@@ -0,0 +1,17 @@
+package ordertracker
+
+class Product {
+
+    static constraints = {
+    }
+
+    String name
+    int brand_id
+    String image
+    String category
+    String characteristic
+    int stock
+    Double price
+    String state
+
+}
