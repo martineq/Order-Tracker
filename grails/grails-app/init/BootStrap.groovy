8c56d55cf2871d409b24bd5611b2042e8b11a046
Mart <martineq@gmail.com>
2016-04-09 21:57:22 -0300

    Agrego carga inicial de productos

--- grails/grails-app/init/BootStrap.groovy
+++ grails/grails-app/init/BootStrap.groovy
@@ -5,6 +5,7 @@ class BootStrap {
     def init = { servletContext ->
 		new UserLoader().load()
         new ClientLoader().load()
+        new ProductLoader().load()
     }
 
     def destroy = {
--- grails/grails-app/init/ordertracker/UserLoader.groovy
+++ grails/grails-app/init/ordertracker/UserLoader.groovy
@@ -6,5 +6,6 @@ class UserLoader {
         new User( username: 'mart',password: '86012', token: 'token2').save()
         new User( username: 'barbara',password: '92071', token: 'token3').save()
         new User( username: 'uriel',password: '93252', token: 'token4').save()
+
     }
 }
