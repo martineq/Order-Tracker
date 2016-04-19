7f8a36a389a71c7b04773e6d159c7cb3ad950bde
uriel kusnesov <uriel_kusnesov@hotmail.com>
2016-04-19 17:24:06 -0300

    rotar catalogo

--- OrderTracker/app/src/main/java/com/example/uriel/ordertracker/App/Activities/DiaryActivity.java
+++ OrderTracker/app/src/main/java/com/example/uriel/ordertracker/App/Activities/DiaryActivity.java
@@ -1,7 +1,6 @@
 package com.example.uriel.ordertracker.App.Activities;
 
 import android.app.Activity;
-import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
