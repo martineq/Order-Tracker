78e2c52e8c0551c95292aab08239186cf4586e0f
Martín Hernán Gómez <martinhgomez@yahoo.com.ar>
2016-04-23 09:59:48 -0300

    [S#3] 3.6 - Date state added to DB domain class ClientOrder

--- grails/grails-app/domain/ordertracker/ClientOrder.groovy
+++ grails/grails-app/domain/ordertracker/ClientOrder.groovy
@@ -1,13 +1,21 @@
 package ordertracker
 
+import ordertracker.constants.ClientStates
+
 class ClientOrder {
 
     static constraints = {
+        seller_id min:1
+        client_id min:1
+        state inList: [ClientStates.NO_VISITADO.toString(), ClientStates.PENDIENTE.toString(), ClientStates.VISITADO.toString()]
+        date min:0
+        total_price min:0.0
     }
 
     int seller_id
     int client_id
     String state
+    long date
     double total_price
 
 }
--- grails/src/test/groovy/ordertracker/ClientOrderSpec.groovy
+++ grails/src/test/groovy/ordertracker/ClientOrderSpec.groovy
@@ -1,11 +1,10 @@
 package ordertracker
 
 import grails.test.mixin.TestFor
+import grails.validation.exceptions.ConstraintException
+import ordertracker.constants.ClientStates
 import spock.lang.Specification
 
-/**
- * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
- */
 @TestFor(ClientOrder)
 class ClientOrderSpec extends Specification {
 
@@ -15,8 +14,95 @@ class ClientOrderSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
+    void "test noExceptionWithWrongData"() {
+        given:
+            def clientOrder = null
+            def exceptionType = ""
+
+        when:
+            try{
+                clientOrder = new ClientOrder(seller_id: 0, client_id: 1, state: ClientStates.VISITADO.toString(), date:0, total_price:1.0)
+            }
+
+            catch ( Exception e ) {
+                exceptionType = e.getClass().getSimpleName()
+            }
+
+        then:
+            clientOrder != null
     }
+
+    void "test wrongSelledID"() {
+        given:
+            def clientOrder = new ClientOrder(seller_id: 0, client_id: 1, state: ClientStates.VISITADO.toString(), date:0, total_price:1.0)
+            def exceptionType = ""
+
+        when:
+            try{
+                clientOrder.save()
+            }
+
+            catch ( Exception e ) {
+                exceptionType = e.getClass().getSimpleName()
+            }
+
+        then:
+            exceptionType == "ConstraintException"
+    }
+
+    void "test negativeTotalPrice"() {
+        given:
+            def clientOrder = new ClientOrder(seller_id: 1, client_id: 1, state: ClientStates.VISITADO.toString(), date:0, total_price:-1.0)
+            def exceptionType = ""
+
+        when:
+            try{
+                clientOrder.save()
+            }
+
+            catch( ConstraintException e ) {
+                exceptionType = e.getClass().getSimpleName()
+            }
+
+        then:
+            exceptionType == "ConstraintException"
+    }
+
+
+    void "test negativeClient_ID"() {
+        given:
+        def clientOrder = new ClientOrder(seller_id: -1, client_id: 1, state: ClientStates.VISITADO.toString(), date:0, total_price:1.0)
+        def exceptionType = ""
+
+        when:
+        try{
+            clientOrder.save()
+        }
+
+        catch( ConstraintException e ) {
+            exceptionType = e.getClass().getSimpleName()
+        }
+
+        then:
+        exceptionType == "ConstraintException"
+    }
+
+    void "test wrongState"() {
+        given:
+            def clientOrder = new ClientOrder(seller_id: 0, client_id: 1, state: "pepe", date:0, total_price:1.0)
+            def exceptionType = ""
+
+        when:
+            try{
+                clientOrder.save()
+            }
+
+            catch ( Exception e ) {
+                exceptionType = e.getClass().getSimpleName()
+            }
+
+        then:
+            exceptionType == "ConstraintException"
+    }
+
 }
