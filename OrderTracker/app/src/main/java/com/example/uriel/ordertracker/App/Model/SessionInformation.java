3bbac789bfe8d30b406907f49dc47883d6c35abe
Martín Hernán Gómez <martinhgomez@yahoo.com.ar>
2016-04-29 12:07:19 -0300

    Refactoring: session information

--- OrderTracker/app/src/main/java/com/example/uriel/ordertracker/App/Model/SessionInformation.java
+++ OrderTracker/app/src/main/java/com/example/uriel/ordertracker/App/Model/SessionInformation.java
@@ -12,11 +12,13 @@ import java.util.Map;
 import java.util.Set;
 
 public class SessionInformation {
-
-    public static SessionInformation sessionInformation = null;
+    private static User user = null;
+    private static SessionInformation sessionInformation = null;
 
     public static SessionInformation InitializeSessionInformation(Context context) {
         SessionInformation.sessionInformation = new SessionInformation(context);
+        SessionInformation.user = SessionInformation.sessionInformation.loadUserInformation();
+
         return SessionInformation.sessionInformation;
     }
 
@@ -24,89 +26,107 @@ public class SessionInformation {
         return SessionInformation.sessionInformation;
     }
 
-
     private SharedPreferences sharedPreferences;
-    private Set<String> options;
 
     public SessionInformation(Context context) {
         this.sharedPreferences = context.getSharedPreferences(Constants.SHARED_PREFERENCES, Context.MODE_PRIVATE);
-        this.loadOptions();
     }
 
-    public void loadOptions() {
-        this.options = new HashSet<>();
+    public static boolean isValidSession() {
+        if ( user == null )
+            SessionInformation.getEditor().loadUserInformation();
 
-        this.options.add(RestService.LOGIN_RESPONSE_ID);
-        this.options.add(RestService.LOGIN_RESPONSE_NAME);
-        this.options.add(RestService.LOGIN_PASSWORD);
-        this.options.add(RestService.LOGIN_TOKEN);
+        return ( user.getUsername().length() != 0 ) && ( user.getPassword().length() != 0 );
     }
 
     public User loadUserInformation() {
-        User user = new User(0,"","");
+        User user = new User(0, "", "" );
         this.loadUserInformation(user);
         return user;
     }
 
-    public void loadUserInformation(User user) {
-        for (String option : this.options) {
-            this.loadUserInformation(user, option);
-        }
+    public void loadUserInformation(User userCopy) {
+        if ( user == null )
+            this.loadSavedUser();
+
+        this.copyUserInformation(userCopy);
     }
 
-    public void saveUserInformation(User user) {
-        for (String option : this.options) {
-            this.saveUserInformation(user, option);
-        }
-    }
-
-    public void saveUserInformation(User user, String option) {
+    public void saveUserInformation(User userInfo) {
+        user = null;
         SharedPreferences.Editor editor = this.sharedPreferences.edit();
 
-        if (option.compareTo(RestService.LOGIN_RESPONSE_ID) == 0)
-            editor.putInt(option, user.getId());
-
-        if (option.compareTo(RestService.LOGIN_RESPONSE_NAME) == 0)
-            editor.putString(option, user.getUsername());
-
-        if (option.compareTo(RestService.LOGIN_PASSWORD) == 0)
-            editor.putString(option, user.getPassword());
-
-        if (option.compareTo(RestService.LOGIN_TOKEN) == 0)
-            editor.putString(option, user.getToken());
+        editor.putInt(RestService.LOGIN_RESPONSE_ID, userInfo.getId());
+        editor.putString(RestService.LOGIN_RESPONSE_NAME, userInfo.getUsername());
+        editor.putString(RestService.LOGIN_PASSWORD, userInfo.getPassword());
+        editor.putString(RestService.LOGIN_TOKEN, userInfo.getToken());
 
         editor.commit();
-    }
-
-    public void loadUserInformation(User user, String option) {
-
-        if (option.compareTo(RestService.LOGIN_RESPONSE_ID) == 0)
-            this.setInteger(user, option);
-
-        if (option.compareTo(RestService.LOGIN_RESPONSE_NAME) == 0)
-            user.setUsername(this.sharedPreferences.getString(option, ""));
-
-        if (option.compareTo(RestService.LOGIN_PASSWORD) == 0)
-            user.setPassword(this.sharedPreferences.getString(option, ""));
-
-        if (option.compareTo(RestService.LOGIN_TOKEN) == 0)
-            user.setToken(this.sharedPreferences.getString(option, ""));
     }
 
     public void removeUserInformation() {
+        user = null;
         SharedPreferences.Editor editor = this.sharedPreferences.edit();
-        for (String option : this.options)
-            editor.remove(option);
+        editor.remove(RestService.LOGIN_RESPONSE_ID);
+        editor.remove(RestService.LOGIN_RESPONSE_NAME);
+        editor.remove(RestService.LOGIN_PASSWORD);
+        editor.remove(RestService.LOGIN_TOKEN);
+
         editor.commit();
     }
 
-    private void setInteger(User user, String option) {
+    private void copyUserInformation(User userCopy) {
+        userCopy.setId(user.getId());
+        userCopy.setUsername(user.getUsername());
+        userCopy.setPassword(user.getPassword());
+        userCopy.setToken(user.getToken());
+    }
+
+    private User loadSavedUser() {
+        SessionInformation.user = new User(0, "", "");
+        user.setId( this.sharedPreferences.getInt( RestService.LOGIN_RESPONSE_ID, 0));
+        user.setUsername( this.sharedPreferences.getString( RestService.LOGIN_RESPONSE_NAME, ""));
+        user.setPassword( this.sharedPreferences.getString( RestService.LOGIN_PASSWORD, ""));
+        user.setToken( this.sharedPreferences.getString( RestService.LOGIN_TOKEN, ""));
+
+        return user;
+    }
+
+    public static int getSessionID() {
         try {
-            user.setId(this.sharedPreferences.getInt(option, -1));
-        }
-        catch (ClassCastException e) {
-            user.setId(-1);
+            return user.getId();
+        } catch (NullPointerException e ) {
+            SessionInformation.sessionInformation.loadSavedUser();
+            return user.getId();
         }
     }
+
+    public static String getSessionUsername() {
+        try {
+            return user.getUsername();
+        } catch (NullPointerException e ) {
+            SessionInformation.sessionInformation.loadSavedUser();
+            return user.getUsername();
+        }
+    }
+
+    public static String getSessionPassword() {
+        try {
+            return user.getUsername();
+        } catch (NullPointerException e ) {
+            SessionInformation.sessionInformation.loadSavedUser();
+            return user.getUsername();
+        }
+    }
+
+    public static String getSessionToken() {
+        try {
+            return user.getToken();
+        } catch (NullPointerException e ) {
+            SessionInformation.sessionInformation.loadSavedUser();
+            return user.getToken();
+        }
+    }
+
 
 }
